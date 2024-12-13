7e82b0e06d59ffbd29939fb7513d4663ff44593b
Joe <joe_ethan2@hotmail.com>
2024-12-13 18:52:45 +0000

    fix: edges are now padded

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { Projects } from "./components/Projects";
 function App() {
   return (
     <div className="min-h-screen min-w-[365px] bg-backgroundlight dark:bg-backgrounddark flex items-center flex-col">
-      <div className="max-w-[1280px] px-4">
+      <div className="max-w-[1280px] p-4">
         <Toaster position="bottom-center" />
         <Header />
         <AboutMe />
