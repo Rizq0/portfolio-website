8e637abae72131538aaa70eb0774bd811270f624
Joe <joe_ethan2@hotmail.com>
2024-12-13 01:35:59 +0000

    fix: styling fixes

--- src/components/ProjectsComponents/IndividualProject.tsx
+++ src/components/ProjectsComponents/IndividualProject.tsx
@@ -6,12 +6,14 @@ interface Props {
 }
 
 export const IndividualProject: React.FC<Props> = ({ project }) => {
+  const { name, description, technologies, link, respository, image } = project;
+
   return (
-    <div className="relative flex flex-col p-4 border rounded-lg min-h-[300px]">
+    <div className="relative flex flex-col p-4 border border-headlinelight dark:border-headlinedark rounded-lg min-h-[300px]">
       <div
         className="absolute inset-0 z-0"
         style={{
-          backgroundImage: `url(${project.image})`,
+          backgroundImage: `url(${image})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
           opacity: "0.2",
@@ -19,17 +21,17 @@ export const IndividualProject: React.FC<Props> = ({ project }) => {
       />
       <div className="relative z-10 flex flex-col h-full gap-4">
         <h2 className="text-headlinelight dark:text-headlinedark font-gabarito text-2xl">
-          {project.name}
+          {name}
         </h2>
         <p className="text-subheadlinelight dark:text-paragraphdark">
-          {project.description}
+          {description}
         </p>
         <div>
           <h3 className="text-headlinelight dark:text-headlinedark font-gabarito text-2xl">
             Technologies
           </h3>
           <ul className="list-disc list-inside text-subheadlinelight dark:text-paragraphdark flex flex-row flex-wrap">
-            {project.technologies.map((technology) => (
+            {technologies.map((technology) => (
               <li key={technology} className="list-none mr-2 my-2">
                 {technology}
               </li>
@@ -37,17 +39,17 @@ export const IndividualProject: React.FC<Props> = ({ project }) => {
           </ul>
         </div>
         <div className="flex flex-row justify-between mt-auto">
-          {project.link === "" ? null : (
+          {link === "" ? null : (
             <a
               className="bg-button text-buttontext font-gabarito text-xl p-2 rounded-lg mt-2 mr-2 flex-1 text-center self-end"
-              href={project.link}
+              href={link}
             >
               Link
             </a>
           )}
           <a
             className="bg-button text-buttontext font-gabarito text-xl p-2 rounded-lg mt-2 flex-1 text-center self-end"
-            href={project.respository}
+            href={respository}
           >
             Repository
           </a>
