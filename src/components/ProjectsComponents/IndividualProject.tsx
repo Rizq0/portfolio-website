5a32a91b23cc98c64345ef4c954483ce75d86eae
Joe <joe_ethan2@hotmail.com>
2024-12-13 15:32:55 +0000

    add: animcations added for project cards

--- src/components/ProjectsComponents/IndividualProject.tsx
+++ src/components/ProjectsComponents/IndividualProject.tsx
@@ -9,7 +9,7 @@ export const IndividualProject: React.FC<Props> = ({ project }) => {
   const { name, description, technologies, link, respository, image } = project;
 
   return (
-    <div className="relative flex flex-col p-4 border border-headlinelight dark:border-headlinedark rounded-lg min-h-[300px]">
+    <div className="relative flex flex-col p-4 border border-headlinelight dark:border-headlinedark rounded-lg min-h-[300px] group hover:cursor-pointer">
       <div
         className="absolute inset-0 z-0"
         style={{
@@ -23,20 +23,22 @@ export const IndividualProject: React.FC<Props> = ({ project }) => {
         <h2 className="text-headlinelight dark:text-headlinedark font-gabarito text-2xl">
           {name}
         </h2>
-        <p className="text-subheadlinelight dark:text-paragraphdark">
-          {description}
-        </p>
-        <div>
-          <h3 className="text-headlinelight dark:text-headlinedark font-gabarito text-2xl">
-            Technologies
-          </h3>
-          <ul className="list-disc list-inside text-subheadlinelight dark:text-paragraphdark flex flex-row flex-wrap">
-            {technologies.map((technology) => (
-              <li key={technology} className="list-none mr-2 my-2">
-                {technology}
-              </li>
-            ))}
-          </ul>
+        <div className="lg:opacity-0 lg:group-hover:opacity-100 transition-opacity duration-300">
+          <p className="text-subheadlinelight dark:text-paragraphdark">
+            {description}
+          </p>
+          <div>
+            <h3 className="text-headlinelight dark:text-headlinedark font-gabarito text-2xl mt-2">
+              Technologies
+            </h3>
+            <ul className="list-disc list-inside text-subheadlinelight dark:text-paragraphdark flex flex-row flex-wrap">
+              {technologies.map((technology) => (
+                <li key={technology} className="list-none mr-2 my-2">
+                  {technology}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
         <div className="flex flex-row justify-between mt-auto">
           {link === "" ? null : (
