48dbdcf91157b7f69b6ad02ce41bc3f1ccd99920
Joe <joe_ethan2@hotmail.com>
2024-12-13 18:52:22 +0000

    add: Reveal utility added

---
+++ src/utilities/Reveal.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useRef } from "react";
+import { motion, useInView, useAnimation } from "framer-motion";
+import { Props } from "../types/utilities";
+
+export const Reveal: React.FC<Props> = ({ children, width }) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const isInView = useInView(ref, { once: true });
+  const mainControls = useAnimation();
+  const slideControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+      slideControls.start("visible");
+    }
+  }, [isInView]);
+
+  const widthValue = width === "fit-content" ? "fit-content" : "w-full";
+  return (
+    <div ref={ref} className={`relative ${widthValue} overflow-hidden`}>
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate={mainControls}
+        transition={{ duration: 0.5, delay: 0.25 }}
+      >
+        {children}
+      </motion.div>
+      <motion.div
+        variants={{
+          hidden: { left: 0 },
+          visible: { left: "100%" },
+        }}
+        initial="hidden"
+        animate={slideControls}
+        transition={{ duration: 0.5, ease: "easeIn" }}
+        style={{
+          position: "absolute",
+          top: 4,
+          bottom: 4,
+          left: 0,
+          right: 0,
+          background: "#ff8906",
+          zIndex: 20,
+        }}
+      />
+    </div>
+  );
+};
