2c7fee7fc761ab4b53bc713693298fc6500bd3af
Joe <joe_ethan2@hotmail.com>
2025-01-06 03:59:21 +0000

    add: larger icons + title text added

--- src/components/ProjectsComponents/IndividualProject.tsx
+++ src/components/ProjectsComponents/IndividualProject.tsx
@@ -49,6 +49,7 @@ export const IndividualProject: React.FC<Props> = ({ project }) => {
               className="text-backgrounddark hover:text-button dark:text-backgroundlight dark:hover:text-button"
               href={link}
               target="_blank"
+              title="Live Site"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -57,8 +58,8 @@ export const IndividualProject: React.FC<Props> = ({ project }) => {
                 stroke="currentColor"
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                width="32"
-                height="32"
+                width="40"
+                height="40"
                 strokeWidth="2"
               >
                 <path d="M9 15l6 -6"></path>
@@ -71,6 +72,7 @@ export const IndividualProject: React.FC<Props> = ({ project }) => {
             className="text-backgrounddark hover:text-button dark:text-backgroundlight dark:hover:text-button"
             href={repository}
             target="_blank"
+            title="GitHub"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -79,8 +81,8 @@ export const IndividualProject: React.FC<Props> = ({ project }) => {
               stroke="currentColor"
               strokeLinecap="round"
               strokeLinejoin="round"
-              width="32"
-              height="32"
+              width="40"
+              height="40"
               strokeWidth="2"
             >
               <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path>
